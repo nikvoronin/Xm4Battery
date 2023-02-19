2e61abc961e79ec4750d3259448973ab9fd22501
Nikolai Voronin <nvoronin@outlook.com>
2023-02-19 21:30:28 +0300

    Define mandatory fields for device property

--- WmiPnp/DeviceProperty.cs
+++ WmiPnp/DeviceProperty.cs
@@ -3,36 +3,26 @@
 namespace WmiPnp;
 
 public class DeviceProperty
-{
-    public string? Data;
-    public readonly string? DeviceId;
-    public readonly string? Key;
-    public readonly string? KeyName; // like DEVPKEY_Device_InLocalMachineContainer
+{    
+    public readonly string DeviceId; // ex. BTHENUM\{0000111E-0000-1000-8000-00805F9B34FB}_VID&0002054C_PID&0D58\8&37022D96&0&F84E17FE9B55_C00000000
+    public readonly string Key; // ex. {540B947E-8B40-45BC-A8A2-6A0B894CBDA2} 8
     public readonly CimType Type;
+    public string? Data;
+    public readonly string? KeyName; // ex. DEVPKEY_Device_InLocalMachineContainer
 
     public DeviceProperty(
-        string? data,
-        string? deviceId,
-        string? key,
-        string? keyName,
-        CimType type )
+        string deviceId,
+        string key,
+        CimType type,
+
+        string? data = null,
+        string? keyName = null )
     {
+        DeviceId = deviceId;
+        Key = key;
+        Type = type;
+
+        KeyName = keyName ?? key;
         Data = data;
-        DeviceId = deviceId;
-        Key = key;
-        KeyName = keyName;
-        Type = type;
-    }
-
-    public DeviceProperty(
-        string? deviceId,
-        string? key,
-        string? keyName,
-        CimType type )
-    {
-        DeviceId = deviceId;
-        Key = key;
-        KeyName = keyName;
-        Type = type;
     }
 }
