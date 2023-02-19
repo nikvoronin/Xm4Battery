de8ec5caa8d0c29332eb4112de970f7878859047
Nikolai Voronin <nvoronin@outlook.com>
2023-02-19 21:14:24 +0300

    Add draft class for device properties

---
+++ WmiPnp/DeviceProperty.cs
@@ -0,0 +1,39 @@
+﻿using System.Management;
+
+namespace WmiPnp
+{
+    public class DeviceProperty
+    {
+        public string? Data;
+        public readonly string? DeviceId;
+        public readonly string? Key;
+        public readonly string? KeyName; // like DEVPKEY_Device_InLocalMachineContainer
+        public readonly CimType Type;
+
+        public DeviceProperty(
+            string? data,
+            string? deviceId,
+            string? key,
+            string? keyName,
+            CimType type )
+        {
+            Data = data;
+            DeviceId = deviceId;
+            Key = key;
+            KeyName = keyName;
+            Type = type;
+        }
+
+        public DeviceProperty(
+            string? deviceId,
+            string? key,
+            string? keyName,
+            CimType type )
+        {
+            DeviceId = deviceId;
+            Key = key;
+            KeyName = keyName;
+            Type = type;
+        }
+    }
+}
