62c3580e9816fcca07a32ad53e4a728014a3aeac
Nikolai Voronin <nvoronin@outlook.com>
2023-02-23 00:34:06 +0300

    Add xm4entity class for xm4 specific operations

---
+++ WmiPnp.Xm4/Xm4Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace WmiPnp.Xm4
+{
+    public class Xm4Entity
+    {
+        private PnpEntity _xm4;
+
+
+        const string PnpEntity_FriendlyName = "WH-1000XM4 Hands-Free AG";
+        const string DeviceProperty_BatteryLevelKey = "{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2";
+        const string DeviceProperty_IsConnected = "{83DA6326-97A6-4088-9453-A1923F573B29} 15";
+        
+        const string DEVPKEY_Bluetooth_LastConnectedTime = "{2BD67D8B-8BEB-48D5-87E0-6CDA3428040A} 11";
+        const string DEVPKEY_Bluetooth_LastConnectedTime2 = "{2BD67D8B-8BEB-48D5-87E0-6CDA3428040A} 5";
+    }
+}
