5aca875b53eb00941ea72c49869da7dc5b9fa790
Nikolai Voronin <nvoronin@outlook.com>
2023-02-27 22:48:10 +0300

    Add IsConnected field to Xm4Entity

--- WmiPnp.Xm4/Xm4Entity.cs
+++ WmiPnp.Xm4/Xm4Entity.cs
@@ -5,36 +5,58 @@ namespace WmiPnp.Xm4
     public class Xm4Entity
     {
         private readonly PnpEntity _xm4;
+        private readonly PnpEntity _handsFree;
 
         // TODO: create options with public access
         private static readonly TimeSpan BatteryLevel_UpdateInterval = TimeSpan.FromMinutes( 5 );
 
-        private Xm4Entity( PnpEntity xm4 )
+        private Xm4Entity( PnpEntity handsFree, PnpEntity xm4 )
         {
+            _handsFree = handsFree;
             _xm4 = xm4;
-            _batteryLevel =
-                new ( _xm4.GetDeviceProperty( PnpEntity.DeviceProperty_BatteryLevel ).Value );
         }
 
         public static Result<Xm4Entity> Create()
-            => CreateBy( PnpEntity_FriendlyName );
+            => CreateBy(
+                HandsFree_PnpEntity_FriendlyName,
+                Headphones_PnpEntity_FriendlyName );
 
-        public static Result<Xm4Entity> CreateBy( string friendlyNameExact )
+        public static Result<Xm4Entity> CreateBy(
+            string handsfreeNameExact,
+            string headphonesNameExact )
         {
-            var result =
+            var hfResult =
                 PnpEntity
-                .ByFriendlyName( friendlyNameExact ?? PnpEntity_FriendlyName );
+                .ByFriendlyName(
+                    handsfreeNameExact
+                    ?? HandsFree_PnpEntity_FriendlyName );
+
+            if ( hfResult.IsFailed )
+                return Result.Fail( $"Can not create {HandsFree_PnpEntity_FriendlyName} entity" );
+
+            var xm4result =
+                PnpEntity
+                .ByFriendlyName(
+                    headphonesNameExact
+                    ?? Headphones_PnpEntity_FriendlyName );
+
+            if ( xm4result.IsFailed )
+                return Result.Fail( $"Can not create {Headphones_PnpEntity_FriendlyName} entity" );
 
             return
-                result.IsSuccess ? new Xm4Entity( result.Value )
-                : Result.Fail( $"Can not create `{PnpEntity_FriendlyName}` entity" );
+                new Xm4Entity(
+                    hfResult.Value,
+                    xm4result.Value );
         }
 
-        public static Result<Xm4Entity> CreateUnsafe( PnpEntity e )
-            => new Xm4Entity( e );
+        public static Result<Xm4Entity> CreateUnsafe(
+            PnpEntity batteryEntity,
+            PnpEntity stateEntity )
+            => new Xm4Entity(
+                batteryEntity,
+                stateEntity );
 
         private DateTimeOffset _batteryLevel_lastUpdate = DateTimeOffset.MinValue;
-        private readonly Lazy<DeviceProperty> _batteryLevel;
         private byte _batteryLevelCached = 0;
         public int BatteryLevel {
             get {
@@ -43,17 +65,32 @@ namespace WmiPnp.Xm4
                     || _batteryLevelCached < 1;
 
                 if ( update ) {
-                    _xm4.UpdateProperty( _batteryLevel.Value );
+                    var batteryLevel =
+                        _handsFree.GetDeviceProperty(
+                            PnpEntity.DeviceProperty_BatteryLevel )
+                        .Value;
 
-                    _batteryLevelCached = (byte)( _batteryLevel.Value.Data ?? 0 );
+                    _batteryLevelCached = (byte)( batteryLevel.Data ?? 0 );
                     _batteryLevel_lastUpdate = DateTimeOffset.UtcNow;
                 }
 
                 return _batteryLevelCached;
             }
         }
-        
-        public const string PnpEntity_FriendlyName
-            = "WH-1000XM4 Hands-Free AG";
+
+        public bool IsConnected {
+            get {
+                var connected =
+                    _xm4.GetDeviceProperty(
+                        PnpEntity.DeviceProperty_IsConnected )
+                    .Value;
+                return (bool)( connected.Data ?? false );
+            }
+        }
+
+        public const string HandsFree_PnpEntity_FriendlyName
+            = "WH-1000XM4 Hands-Free AG"; // Battery level related
+        public const string Headphones_PnpEntity_FriendlyName
+            = "WH-1000XM4"; // Headphones state related
     }
 }
