7def60958c919e82b6f874e6df662ee486ec8c1e
Nikolai Voronin <nvoronin@outlook.com>
2023-02-28 23:13:22 +0300

    Clean up a little

--- Xm4BatteryLevelConsole/Program.cs
+++ Xm4BatteryLevelConsole/Program.cs
@@ -16,39 +16,5 @@ while ( !Console.KeyAvailable ) {
 }
 
 if ( !xm4.IsConnected ) {
-    Console.WriteLine( $"Last connected time: ???" );
+    Console.WriteLine( $"\nLast connected time: ???" );
 }
-
-//PnpEntity
-//    .ByFriendlyName(
-//        Xm4Entity.PnpEntity_FriendlyName )
-//    .IfSome( e => ProcessEntity( e ) );
-
-//void ProcessEntity( Result<PnpEntity> xm4entity )
-//{
-//    var xm4 = xm4entity.Value;
-
-//    Console.WriteLine( $"--> {xm4.Name}: {xm4.Description}" );
-//    var pr = xm4.GetDeviceProperty( PnpEntity.DeviceProperty_BatteryLevel );
-
-//    foreach ( var p in xm4.UpdateProperties() )
-//        Console.WriteLine( $"{p.KeyName}: {p.Data}" );
-
-//    Console.WriteLine();
-
-//    while ( !Console.KeyAvailable ) {
-//        var level =
-//            pr
-//            .Some( dp => {
-//                xm4.UpdateProperty( dp );
-//                return dp.Data;
-//            } )
-//            .None( () => "[x] Key not found" );
-
-//        Console.Write( $"\rBattery Level: {level}%" );
-
-//        Thread.Sleep( 1000 );
-//    }
-
-//    Console.WriteLine();
-//}
