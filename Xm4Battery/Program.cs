06e23f2286bafcca19cf605e7790464962e1a119
Nikolai Voronin <nvoronin@outlook.com>
2024-06-18 19:31:19 +0300

    Enlarge icon size

--- Xm4Battery/Program.cs
+++ Xm4Battery/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Security.Principal;
 using WmiPnp.Xm4;
 
@@ -10,6 +11,7 @@ internal static class Program
     static int Main()
     {
         ApplicationConfiguration.Initialize();
+        Application.SetHighDpiMode( HighDpiMode.PerMonitorV2 );
 
         var xm4result = Xm4Entity.Create();
         if (xm4result.IsFailed)
@@ -17,8 +19,8 @@ internal static class Program
 
         Xm4Entity xm4 = xm4result.Value;
 
-        NotifyIcon notifyIconCtrl =
-            new() {
+        var notifyIconCtrl =
+            new NotifyIcon {
                 Text = NotifyIcon_BatteryLevelTitle,
                 Visible = true,
                 Icon = CreateIconForLevel( DisconnectedLevel ),
@@ -44,12 +46,13 @@ internal static class Program
             };
 
         statePoll.Start();
-
         Application.Run();
+        statePoll.Stop();
 
         notifyIconCtrl.Visible = false;
+        var prevIcon = notifyIconCtrl.Icon;
         notifyIconCtrl.Dispose();
-        statePoll.Stop();
+        DestroyIcon( prevIcon.Handle );
 
         return (int)ErrorLevel.ExitOk;
     }
@@ -118,7 +121,7 @@ internal static class Program
     }
 
     static readonly Font _notifyIconFont
-        = new( "Segoe UI", 10, FontStyle.Regular );
+        = new( "Segoe UI", 124, FontStyle.Regular );
 
     private static Icon CreateIconForLevel( int level )
     {
@@ -131,28 +134,40 @@ internal static class Program
         g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
         g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
 
-        level = 40;
-
-        // icon background
-        var brush =
+        // icon background color
+        var iconBackgroundBrush =
             level switch {
-                > 0 and <= 10 => Brushes.Red,
-                > 0 and <= 20 => Brushes.Orange,
-                > 0 and <= 30 => Brushes.Yellow,
-                <= 0 => Brushes_WhiteA100,
-                _ => Brushes.White
+                <= DisconnectedLevel => Brushes.Transparent,
+                <= 10 => Brushes.Red,
+                <= 20 => Brushes.Orange,
+                <= 30 => Brushes.Yellow,
+                _ => Brushes.White // 40..100(F)
+            };
+
+        // icon text color
+        var iconTextBrush =
+            level switch {
+                <= DisconnectedLevel => Brushes.WhiteSmoke,
+                //<= 10 => Brushes.Magenta,
+                //<= 20 => Brushes.Cyan,
+                _ => Brushes.Black
             };
 
         g.FillRectangle(
-            brush,
-            0, 0, iw - 1, ih - 1 );
+            iconBackgroundBrush,
+            0, 0, iw, ih );
+
+        g.DrawRectangle(
+            Pens_WhiteSmokeW24,
+            0, 0, iw, ih );
 
         // icon text: battery level or status
         var iconText =
             level switch {
+                <= DisconnectedLevel => "X",
+                <= 10 => "!",
                 100 => "F", // Full charged
-                > 0 and < 100 => level.ToString()[..^1],
-                _ => "X" // Disconnected
+                _ => level.ToString()[..^1], // One digit of charge level 1..9
             };
 
         var sizeS =
@@ -163,7 +178,7 @@ internal static class Program
         g.DrawString(
             iconText,
             _notifyIconFont,
-            Brushes.Black,
+            iconTextBrush,
             iw / 2 - sizeS.Width / 2,
             ih / 2 - sizeS.Height / 2 );
 
@@ -192,10 +207,14 @@ internal static class Program
         items[ConnectCtxMenuItemName].Enabled = !connected;
         items[DisconnectCtxMenuItemName].Enabled = connected;
 
+        var prevIcon = notifyIconCtrl.Icon;
+
         notifyIconCtrl.Icon =
             CreateIconForLevel(
                 connected ? level
                 : DisconnectedLevel );
+
+        DestroyIcon( prevIcon.Handle );
 
         var at =
             connected ? string.Empty
@@ -205,19 +224,19 @@ internal static class Program
             $"{NotifyIcon_BatteryLevelTitle} ⚡{level}%{at}";
     }
 
-    static readonly SolidBrush Brushes_WhiteA100 =
-        new( Color.FromArgb( 100, Color.White ) );
+    static readonly Pen Pens_WhiteSmokeW24 =
+        new( Color.WhiteSmoke, 24f );
 
     const string ConnectCtxMenuItemName = nameof( ConnectCtxMenuItemName );
     const string DisconnectCtxMenuItemName = nameof( DisconnectCtxMenuItemName );
-    const int NotifyIconDefault_WidthPx = 20;
-    const int NotifyIconDefault_HeightPx = 20;
+    const int NotifyIconDefault_WidthPx = 256;
+    const int NotifyIconDefault_HeightPx = 256;
 
     const int DisconnectedLevel = 0;
     const string NotifyIcon_BatteryLevelTitle = "XM4 Battery Level";
 
     const string AppName = "Xm4Battery";
-    const string AppVersion = "4.6.17";
+    const string AppVersion = "4.6.18";
     const string GithubProjectUrl = "https://github.com/nikvoronin/WmiPnp";
 
     internal enum ErrorLevel
@@ -225,4 +244,7 @@ internal static class Program
         ExitOk = 0,
         Xm4NotFound = 1
     }
+
+    [DllImport( "user32.dll", CharSet = CharSet.Unicode )]
+    static extern bool DestroyIcon( IntPtr handle );
 }
